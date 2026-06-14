c32e9c548d031bb3ba3b10b9ce41f1cde372381f
Sean Bush <sbus139@wgu.edu>
2026-06-14 16:46:59 -0500

    Move temp table creation and deletion into master setup script

--- setup.sql
+++ setup.sql
@@ -1,8 +1,24 @@
-\i setup/staging.sql
+CREATE TABLE temp_raw_data (
+	BookID integer,
+	Title text,
+	Author text,
+	Genre text,
+	BranchID integer,
+	BranchName text,
+	BorrowerID integer,
+	BorrowerName text,
+	BorrowDate text,
+	ReturnDate text
+);
+
+\copy temp_raw_data FROM 'city_library_data.csv' CSV HEADER;
+
 \i setup/import_author.sql
 \i setup/import_genre.sql
 \i setup/import_books.sql
 \i setup/import_branch.sql
 \i setup/import_inventory.sql
 \i setup/import_customer.sql
-\i setup/import_checkout.sqls
+\i setup/import_checkout.sql
+
+DROP TABLE temp_raw_data;
