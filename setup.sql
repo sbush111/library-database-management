aa0b0b6d21a852fb5f9c8132cf5b8592eeecbcb0
Sean Bush <sbus139@wgu.edu>
2026-06-15 14:30:01 -0500

    Organize files

--- setup.sql
+++ setup.sql
@@ -11,14 +11,14 @@ CREATE TABLE temp_raw_data (
 	ReturnDate text
 );
 
-\copy temp_raw_data FROM 'setup/city_library_data.csv' CSV HEADER;
+\copy temp_raw_data FROM 'dbsetup/city_library_data.csv' CSV HEADER;
 
-\i setup/import_author.sql
-\i setup/import_genre.sql
-\i setup/import_book.sql
-\i setup/import_branch.sql
-\i setup/import_inventory.sql
-\i setup/import_member.sql
-\i setup/import_checkout.sql
+\i dbsetup/import_author.sql
+\i dbsetup/import_genre.sql
+\i dbsetup/import_book.sql
+\i dbsetup/import_branch.sql
+\i dbsetup/import_inventory.sql
+\i dbsetup/import_member.sql
+\i dbsetup/import_checkout.sql
 
 DROP TABLE temp_raw_data;
